df72ef72a8955ac819163b3c658c1f598a40e0cb
Linar Daminov <linar.daminov@yandex.ru>
2022-12-09 19:29:37 +0500

    FirstCommit

---
+++ src/main/java/com/example/lessontwocoursethree/LessonTwoCourseThreeApplication.java
@@ -0,0 +1,13 @@
+package com.example.lessontwocoursethree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonTwoCourseThreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LessonTwoCourseThreeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lessontwocoursethree/LessonTwoCourseThreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lessontwocoursethree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessonTwoCourseThreeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
